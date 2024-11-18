fcf3890031aafbf2b6b44c2501f346a3dc0d5061
Gustavo M Nogueira <gustavo.mnogueira95@gmail.com>
2024-11-18 20:49:56 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./_components/ui/button";
+
 function Home() {
   return (
     <div className="flex items-center justify-center text-red-600">
-      <h1> hello world</h1>
+      <Button>Hello World</Button>
     </div>
   );
 }
